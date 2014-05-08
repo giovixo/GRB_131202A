9e836e3266494b236e7557c962e77ae2b9244f50
Giovanni De Cesare <decesare.giovanni@gmail.com>
2014-05-08 10:55:13 +0200

    Ora legge dalla riga di comando il nome del file di eventi

--- data_reduction/evselect.sh
+++ data_reduction/evselect.sh
@@ -1,1 +1,2 @@
-evselect table=mos1.fits withimageset=yes imageset=image.fits xcolumn=X ycolumn=Y imagebinning=imageSize ximagesize=600 yimagesize=600
+#!/bin/bash
+evselect table=$1 withimageset=yes imageset=image.fits xcolumn=X ycolumn=Y imagebinning=imageSize ximagesize=600 yimagesize=600
